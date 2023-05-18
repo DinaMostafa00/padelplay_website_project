3ef7e1eb5a14e791f36e2676a7aa006e4098bf04
DinaMo96 <93649274+DinaMostafa96@users.noreply.github.com>
2023-05-18 12:47:12 +0300

    blog
    dfghjk

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,42 +1,3 @@
-// import { getBlog } from "@/sanity/sanity-utils";
-// import React from "react";
-// import Image from "next/image";
-// import Link from "next/link";
-// import blog from "@/sanity/schemas/blog-schema";
-
-// export default async function Blog() {
-//   const blog = await getBlog();
-
-//   return (
-//     <div>
-//       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-//         {blog.map((blog) => (
-//           <Link
-//             href={`/blog/${blog.slug}`}
-//             key={blog._id}
-//             className="border-2 border-yellow-500  p-1 hover:scale-105 hover:border-white transition"
-//           >
-//             {blog.image && (
-//               <div>
-//                 <Image
-//                   src={blog.image}
-//                   alt={blog.name}
-//                   width={750}
-//                   height={300}
-//                   className="object-cover  "
-//                 />
-//               </div>
-//             )}
-//             <div className="">{blog.name}</div>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-//////////////////////////
-
 import { getBlog } from "@/sanity/sanity-utils";
 import React from "react";
 import Image from "next/image";
@@ -50,10 +11,10 @@ export default async function Blog() {
   const otherBlogs = blogs.slice(1); // Remove the latest blog from the array
 
   return (
-    <div>
-      <div className="flex justify-center py-20 hover:scale-105">
+    <div className="defaultSpacing backGround">
+      <div className="flex justify-center  py-10 lg:py-20 hover:scale-105">
         <Link href={`/blog/${latestBlog.slug}`} key={latestBlog._id}>
-          <div className="border-2 border-yellow-500  p-1 hover:scale-105 hover:border-white transition">
+          <div className="border-2 border-yellow-500 p-2  lg:p-5 hover:scale-105 hover:border-white transition ">
             {latestBlog.image && (
               <div className="relative">
                 <Image
@@ -61,11 +22,15 @@ export default async function Blog() {
                   alt={latestBlog.name}
                   width={750}
                   height={300}
-                  className="object-cover"
+                  className="object-fit"
                 />
 
-                <div className="absolute bottom-0 left-0 w-full h-1/6 bg-black bg-opacity-50 text-white font-bold">
-                  {latestBlog.name}
+                <div className="latestBlog">
+                  <p>{latestBlog.name}</p>
+
+                  <span className="material-symbols-outlined text-yellow-500">
+                    arrow_forward_ios
+                  </span>
                 </div>
               </div>
             )}
@@ -73,28 +38,32 @@ export default async function Blog() {
         </Link>
       </div>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 lg:py-8">
         {otherBlogs.map((blog) => (
           <Link
             href={`/blog/${blog.slug}`}
             key={blog._id}
-            className="border-2 border-yellow-500 p-1 hover:scale-105 hover:border-white transition"
+            className="border-2 border-yellow-500 p-3 hover:scale-105 hover:border-white transition"
           >
             {blog.image && (
-              <div>
+              <div className="relative">
                 <Image
                   src={blog.image}
                   alt={blog.name}
                   width={750}
                   height={300}
-                  className="object-cover"
+                  className="oobject-fit"
                 />
+                <div className="otherBlog">
+                  <h2 className="">{blog.name}</h2>
+                </div>
               </div>
             )}
-            <div>{blog.name}</div>
           </Link>
         ))}
       </div>
     </div>
   );
 }
+
+///write a reminder of the size
