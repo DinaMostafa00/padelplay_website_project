e117238b7ea913105eca0caaf3a96b0f6c7af320
DinaMo96 <93649274+DinaMostafa96@users.noreply.github.com>
2023-05-21 23:36:03 +0300

    price
    nm

---
+++ app/(site)/priceing/page.tsx
@@ -0,0 +1,161 @@
+export default async function pricePage() {
+  return (
+    <div className="defaultSpacing backGround py-10">
+      <table className=" w-full  border-collapse border-2  border-yellow-500">
+        <thead>
+          <tr className=" ">
+            <th className=" p-2">Features</th>
+            <th className=" p-2">BASIC</th>
+            <th className=" p-2"> ESSENTIAL</th>
+            <th className=" p-2">PREMIUM</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="   px-4 py-2  priceTableCol">Camera </td>
+            <td className=" px-4 py-2 priceTableCol ">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              {" "}
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">dynamic scoreboard</td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Ads in stream</td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">
+              QR-code for spontaneous streams
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">
+              remote service and surveillance over the equipment
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr className="">
+            <td className=" px-4 py-2 priceTableCol">commentators </td>
+            <td className=" px-4 py-2 priceTableCol">__</td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Youtube channel </td>
+            <td className=" px-4 py-2 priceTableCol">__</td>
+            <td className=" px-4 py-2 priceTableCol">__</td>
+            <td className=" px-4 py-2 priceTableCol">
+              <span className="material-symbols-outlined">done</span>
+            </td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Contract period </td>
+            <td className=" px-4 py-2 priceTableCol">12 month</td>
+            <td className=" px-4 py-2 priceTableCol">12 month</td>
+            <td className=" px-4 py-2 priceTableCol">12 month</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">12 month </td>
+            <td className=" px-4 py-2 priceTableCol">Row 2, Column 2</td>
+            <td className=" px-4 py-2 priceTableCol">Row 2, Column 3</td>
+            <td className=" px-4 py-2 priceTableCol">Row 2, Column 4</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">CTA </td>
+            <td className=" px-4 py-2 priceTableCol">Form &amp; Call</td>
+            <td className=" px-4 py-2 priceTableCol">Form &amp; Call</td>
+            <td className=" px-4 py-2 priceTableCol">Form&amp; Call</td>
+          </tr>
+
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Price </td>
+            <td className=" px-4 py-2 priceTableCol">750kr/month</td>
+            <td className=" px-4 py-2 priceTableCol">1000kr/month</td>
+            <td className=" px-4 py-2 priceTableCol">1500kr/month</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">-- </td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Extras</td>
+            <td className=" px-4 py-2 priceTableCol">
+              Commentators (350kr/month) Youtube channel (600krmonth)
+            </td>
+            <td className=" px-4 py-2 priceTableCol">
+              Youtube channel (600krmonth)
+            </td>
+            <td className=" px-4 py-2 priceTableCol">__</td>
+          </tr>
+
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Total Price</td>
+            <td className=" px-4 py-2 priceTableCol">1 700kr/month</td>
+            <td className=" px-4 py-2 priceTableCol">1600kr/month</td>
+            <td className=" px-4 py-2 priceTableCol">1500kr/month</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">Special offer</td>
+            <td className=" px-4 py-2 priceTableCol">24 month - 20% off</td>
+            <td className=" px-4 py-2 priceTableCol">24 month - 20% off</td>
+            <td className=" px-4 py-2 priceTableCol">24 month - 20% off</td>
+          </tr>
+          <tr>
+            <td className=" px-4 py-2 priceTableCol">--</td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+            <td className=" px-4 py-2 priceTableCol">GET IT NOW</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
